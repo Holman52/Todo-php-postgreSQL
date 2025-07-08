eb05014ffc04fe98d281176993889445de5592bc
Ivan Holm <vinokur4515@gmail.com>
2025-07-09 01:30:40 +0300

    отправка формы и отчитска

--- src/components/Form/FormPost.jsx
+++ src/components/Form/FormPost.jsx
@@ -6,8 +6,48 @@ import Button from '../Button';
 
 export default function FormPost() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formData, setFormData] = useState({
+    task: '',
+    num: '',
+  })
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    setIsSubmitting(true)
+    try {
+      const response = await fetch('http://localhost/api/test/post_method.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
 
+      if (!response.ok) {
+        throw new Error('Ошибка при отправке формы');
+      }
+    }
+    catch(error){
+      console.log(error)
+    }
+    finally {
+      setIsSubmitting(false);
+      setFormData({
+        task: '',
+        num: '',
+      })
+  }
 
+  
+}
+console.log(formData)
   return (
     <div className='form'>
       <div 
@@ -19,28 +59,32 @@ export default function FormPost() {
       </div>
       
       <div className={`form-content ${isOpen ? 'open' : ''}`}>
-        <form action="http://localhost/api/test/post_method.php" method='POST' className='form-post'>
+        <form onSubmit={handleSubmit}>
           <div className='form-post__header-block'>
             <label htmlFor="" className='form-post__header-block__label'>Введите задачу</label>
             <Input
               type='text' 
-              name='text' 
+              name='task' 
               id='text' 
               className='form-post__header-block__input' 
               placeholder='Название задачи'
+              value={formData.task}
+              onChange={handleChange}
             />
           </div>
            <div className='form-post__header-block'>
             <label htmlFor="" className='form-post__header-block__label'>Введите степень важности</label>
             <Input
               type='number' 
-              name='number-importance' 
+              name='num' 
               id='text' 
               className='form-post__header-block__input-num'
+              value={formData.num}
+              onChange={handleChange}
             />
           </div>
           <div  className='form-post__button' >
-            <Button type='submit' title='Cоздать'/>
+            <Button type='submit' title={isSubmitting ? 'Отправка...' : 'Отправить'}/>
           </div>
         </form>
       </div>
--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import '../Input.scss'
 export default function Input(props) {
   return (
-    <input type={props.type} name={props.name} id={props.id} className={`${"input "} ${props.className}`} min={1} placeholder={props.placeholder} />
+    <input 
+      type={props.type} 
+      name={props.name} 
+      id={props.id} 
+      className={`${"input "} ${props.className}`} 
+      min={1} placeholder={props.placeholder}    
+      value={props.value}
+      onChange={props.onChange}/>
   )
 }
