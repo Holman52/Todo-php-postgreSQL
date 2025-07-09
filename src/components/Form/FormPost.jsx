2e860a61c800155b4e2ee95dcf5a855eb670f3f6
Ivan Holm <vinokur4515@gmail.com>
2025-07-09 11:12:41 +0300

    данные заносятся в бд

--- src/components/Form/FormPost.jsx
+++ src/components/Form/FormPost.jsx
@@ -25,7 +25,7 @@ export default function FormPost() {
       const response = await fetch('http://localhost/api/test/post_method.php', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Accept': 'application/json',
         },
         body: JSON.stringify(formData)
       });
